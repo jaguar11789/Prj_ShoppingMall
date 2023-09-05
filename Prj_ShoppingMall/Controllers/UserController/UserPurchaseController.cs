b59fa17a88b880564fa3bb48e9f8e7bdd66c3a12
jaguar11789 <jaguar11789@naver.com>
2023-09-05 17:09:01 +0900

    KSW_Purchase

---
+++ Prj_ShoppingMall/Controllers/UserController/UserPurchaseController.cs
@@ -0,0 +1,80 @@
+﻿using Prj_ShoppingMall.Models.AdminService;
+using Prj_ShoppingMall.Models.Info;
+using Prj_ShoppingMall.Models.UserService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_ShoppingMall.Controllers.UserController
+{
+    public class UserPurchaseController : Controller
+    {
+        UserItemService _userItemService = new UserItemService();
+        AdminCouponService _adminCouponService = new AdminCouponService();
+        // GET: UserPurchase
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 배송지 수정
+        [HttpPost]
+        public JsonResult AddrUpdate(UserInfo objUserInfo)
+        {
+            CheckInfo objCheckInfo = null;
+
+            if (Session["strUserId"] != null)
+            {
+                objUserInfo.strUserId = Session["strUserId"].ToString();
+                objCheckInfo = _userItemService.userAddrUpdate(objUserInfo, objUserInfo.strUserId);
+
+                return Json(objCheckInfo);
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        // 쿠폰 유효성 검사
+        [HttpPost]
+        public JsonResult CheckCoupon(CouponInfo objCouponInfo)
+        {
+            CheckInfo objCheckInfo = null;
+
+            if (Session["strUserId"] != null)
+            {
+                objCheckInfo = _adminCouponService.couponValidation(objCouponInfo);
+
+                return Json(objCheckInfo);
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        // 상품구매
+        [HttpPost]
+        public JsonResult PurchaseItem(UserInfo objUserInfo, ItemInfo objItemInfo, string strItemColor, string strItemSize, long? lngCpCode, string strReceiveUserId)
+        {
+            CheckInfo objResult = null;
+            long cpCodeValue    = lngCpCode ?? 0;
+
+            if (Session["strUserId"] != null)
+            {
+                objUserInfo.strUserId = Session["strUserId"].ToString();
+
+                objResult = _userItemService.purchaseItem(objUserInfo, objItemInfo, strItemColor, strItemSize, cpCodeValue, strReceiveUserId);
+
+                return Json(objResult);
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+        }
+    }
+}
--- Prj_ShoppingMall/Models/UserService/UserItemService.cs
+++ Prj_ShoppingMall/Models/UserService/UserItemService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Prj_ShoppingMall.Models.Common;
+using Prj_ShoppingMall.Models.Info;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +13,126 @@ namespace Prj_ShoppingMall.Models.UserService
     public class UserItemService
     {
         private static string strConnectionString = ConfigurationManager.ConnectionStrings["strConnectionString"].ConnectionString;
+
+        // 배송지 수정
+        public CheckInfo userAddrUpdate(UserInfo objUserInfo, string strUserId)
+        {
+            int intRetVal    = -1;
+            string strRetMsg = null;
+            CheckInfo objCheckInfo = null;
+
+            using (SqlConnection conn = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UP_USERADDR_TX_UPD", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@pi_strUserId",     SqlDbType.VarChar).Value = strUserId;
+                    cmd.Parameters.Add("@pi_strUserName",   SqlDbType.VarChar).Value = objUserInfo.strUserName;
+                    cmd.Parameters.Add("@pi_strAddrPost",   SqlDbType.VarChar).Value = objUserInfo.strAddrPost;
+                    cmd.Parameters.Add("@pi_strAddrBase",   SqlDbType.VarChar).Value = objUserInfo.strAddrBase;
+                    cmd.Parameters.Add("@pi_strAddrDtl",    SqlDbType.VarChar).Value = objUserInfo.strAddrDtl;
+
+                    cmd.Parameters.Add("@pi_strUserTelNum", SqlDbType.VarChar).Value = objUserInfo.strUserTelNum;
+
+                    SqlParameter outputParamRetVal  = new SqlParameter();
+                    outputParamRetVal.ParameterName = "@po_intRetVal";
+                    outputParamRetVal.SqlDbType     = SqlDbType.Int;
+                    outputParamRetVal.Direction     = ParameterDirection.Output;
+                    cmd.Parameters.Add(outputParamRetVal);
+
+                    SqlParameter outputParamRetMsg  = new SqlParameter();
+                    outputParamRetMsg.ParameterName = "@po_strRetMsg";
+                    outputParamRetMsg.SqlDbType     = SqlDbType.VarChar;
+                    outputParamRetMsg.Direction     = ParameterDirection.Output;
+                    outputParamRetMsg.Size          = 256;
+                    cmd.Parameters.Add(outputParamRetMsg);
+
+                    conn.Open();
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+
+                        intRetVal = Convert.ToInt32(cmd.Parameters["@po_intRetVal"].Value);
+                        strRetMsg = Convert.ToString(cmd.Parameters["@po_strRetMsg"].Value);
+
+                        objCheckInfo = new CheckInfo
+                        {
+                            intRetVal = intRetVal,
+                            strRetMsg = strRetMsg
+                        };
+                        return objCheckInfo;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Info("[UserItemService][userAddrUpdate] ex : " + ex);
+
+                        return objCheckInfo;
+                    }
+                }
+            }
+        }
+
+        // 상품 구매
+        public CheckInfo purchaseItem(UserInfo objUserInfo, ItemInfo objItemInfo ,string strItemColor, string strItemSize, long lngCpCode, string strReceiveUserId)
+        {
+            int intRetVal    = -1;
+            string strRetMsg = null;
+            CheckInfo objCheckInfo = null;
+
+            using (SqlConnection conn = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UP_PURCHASE_TX_INS", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@pi_strUserId",        SqlDbType.VarChar).Value = objUserInfo.strUserId;
+                    cmd.Parameters.Add("@pi_intItemNo",        SqlDbType.Int).Value     = objItemInfo.intItemNo;
+                    cmd.Parameters.Add("@pi_strItemName",      SqlDbType.VarChar).Value = objItemInfo.strItemName;
+                    cmd.Parameters.Add("@pi_strItemColor",     SqlDbType.VarChar).Value = strItemColor;
+                    cmd.Parameters.Add("@pi_strItemSize",      SqlDbType.VarChar).Value = strItemSize;
+
+                    cmd.Parameters.Add("@pi_intItemPrice",     SqlDbType.Int).Value     = objItemInfo.intItemPrice;
+                    cmd.Parameters.Add("@pi_strReceiveUserId", SqlDbType.VarChar).Value = strReceiveUserId;
+                    cmd.Parameters.Add("@pi_intCpCode",        SqlDbType.BigInt).Value  = lngCpCode;
+
+                    SqlParameter outputParamRetVal  = new SqlParameter();
+                    outputParamRetVal.ParameterName = "@po_intRetVal";
+                    outputParamRetVal.SqlDbType     = SqlDbType.Int;
+                    outputParamRetVal.Direction     = ParameterDirection.Output;
+                    cmd.Parameters.Add(outputParamRetVal);
+
+                    SqlParameter outputParamRetMsg  = new SqlParameter();
+                    outputParamRetMsg.ParameterName = "@po_strRetMsg";
+                    outputParamRetMsg.SqlDbType     = SqlDbType.VarChar;
+                    outputParamRetMsg.Direction     = ParameterDirection.Output;
+                    outputParamRetMsg.Size          = 256;
+                    cmd.Parameters.Add(outputParamRetMsg);
+
+                    conn.Open();
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+
+                        intRetVal = Convert.ToInt32(cmd.Parameters["@po_intRetVal"].Value);
+                        strRetMsg = Convert.ToString(cmd.Parameters["@po_strRetMsg"].Value);
+
+                        objCheckInfo = new CheckInfo
+                        {
+                            intRetVal = intRetVal,
+                            strRetMsg = strRetMsg
+                        };
+                        return objCheckInfo;
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Info("[UserItemService][userAddrUpdate] ex : " + ex);
+
+                        return objCheckInfo;
+                    }
+                }
+            }
+        }
     }
 }
